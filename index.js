7e75f701167dba7e6f7867d55dd34b56581f84d0
quiwaan <quiwaan@me.com>
2019-01-31 12:50:22 -0800

    added user model and index.js did not add all modules yet

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const expressJwt = require('express-jwt')
+
+
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+
+
+function fromRequest(req){
+    if(req.body.headers.Authrorization && req.body.headers.Authrorization.split(' ')[0] === 'Bearer'){
+        return req.body.headers.Authrorization.split(' ')[1]
+    }
+    return null
+}
+
+app.use('/auth', expressJwt({
+	secret: process.env.JWT_SECRET,
+	getToken: fromRequest
+}).unless({
+	path:[{ url: '/auth/login', methods: ['POST'] }, { url: '/auth/signup', methods: ['POST'] }] 
+}), require('./controllers/auth'));
+
+app.get('*', function(req, res, next) {
+	res.status(404).send({ message: 'Not Found' });
+});
+
+app.listen(process.env.PORT || 3000);
