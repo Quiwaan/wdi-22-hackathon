cf1024c83c94dee40e6ff72a9cde7ec4e8c30e7e
jamesliudotcc <james@jamesliu.cc>
2019-01-31 15:46:48 -0800

    edited model index

--- seed-db.js
+++ seed-db.js
@@ -1,19 +1,32 @@
 const db = require('./models');
 
-const createTestDb = async () => {
+const createHouseStark = async () => {
+  const house1 = await db.House.create({
+    name: 'Stark',
+  });
+
   const user1 = await db.User.create({
-    name: 'Test User 1',
-    email: 'test@jamesliu.cc',
+    name: 'Neddard',
+    email: 'test13@jamesliu.cc',
     password: 'asdfasdf',
   });
 
   const user2 = await db.User.create({
-    name: 'Test User 2',
-    email: 'test2@jamesliu.cc',
+    name: 'Arya',
+    email: 'test14@jamesliu.cc',
     password: 'qwerasdf',
-    housemates: [user1._id],
   });
-  user1.
+  await db.User.findById(user1._id, (err, user1) => {
+    console.log(user1);
+    if (err) return err;
+    db.House.update({ name: 'Stark' }, { $push: user1._id });
+  });
+  await db.User.findById(user2._id, (err, user2) => {
+    console.log(user2);
+    if (err) return err;
+    db.House.update({ name: 'Stark' }, { $push: user2._id });
+  });
+  console.log(house1);
 };
 
-createTestDb();
+createHouseStark().then(console.log('Done'));
