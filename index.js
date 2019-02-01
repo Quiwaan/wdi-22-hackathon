2fa7fe01fdb54659c23bf2cf10cacf5141987aba
jamesliudotcc <james@jamesliu.cc>
2019-01-31 16:40:04 -0800

    added mongo stuff

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const express = require('express')
-const expressJwt = require('express-jwt')
+const express = require('express');
+const expressJwt = require('express-jwt');
 
-
+require('dotenv').config();
 
 const app = express();
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-
-function fromRequest(req){
-    if(req.body.headers.Authrorization && req.body.headers.Authrorization.split(' ')[0] === 'Bearer'){
-        return req.body.headers.Authrorization.split(' ')[1]
-    }
-    return null
+function fromRequest(req) {
+  if (
+    req.body.headers.Authrorization &&
+    req.body.headers.Authrorization.split(' ')[0] === 'Bearer'
+  ) {
+    return req.body.headers.Authrorization.split(' ')[1];
+  }
+  return null;
 }
 
-app.use('/auth', expressJwt({
-	secret: process.env.JWT_SECRET,
-	getToken: fromRequest
-}).unless({
-	path:[{ url: '/auth/login', methods: ['POST'] }, { url: '/auth/signup', methods: ['POST'] }] 
-}), require('./controllers/auth'));
+app.use(
+  '/auth',
+  expressJwt({
+    secret: process.env.JWT_SECRET,
+    getToken: fromRequest,
+  }).unless({
+    path: [
+      { url: '/auth/login', methods: ['POST'] },
+      { url: '/auth/signup', methods: ['POST'] },
+    ],
+  }),
+  require('./controllers/auth')
+);
 
 app.get('*', function(req, res, next) {
-	res.status(404).send({ message: 'Not Found' });
+  res.status(404).send({ message: 'Not Found' });
 });
 
 app.listen(process.env.PORT || 3000);
